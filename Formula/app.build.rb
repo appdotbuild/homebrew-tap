45474ba7d4e04d1892d403b74f4d46d25fe93d8b
Alexander Bayandin <alexander@bayandin.dev>
2025-06-03 15:08:15 +0000

    app.build: add 1.13.0 bottle.

--- Formula/app.build.rb
+++ Formula/app.build.rb
@@ -5,6 +5,14 @@ class AppBuild < Formula
   sha256 "4487d267fe2508c508b5dd444ea556625e5e0d96dee006865cf09415824f4a20"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/appdotbuild/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d30a612c5a71bde857fc54be0b9067e5d37806b0d2d5d7267a45fb31075ef99"
+    sha256 cellar: :any_skip_relocation, ventura:       "c12df5d4f296a0ac8ca4b7b3b1d5cc289b5e3f86a831a6cc11cab24dc834430a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "af6154fb3c5647bec350d476bd86b8478673089919a7a83e13f027f1c7e751c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb7e590fec05568b3827226e3ff9d53d207b1e0d79f9f8976b4afffbd7fc0362"
+  end
+
   depends_on "node"
 
   def install
